bb9da216bc5d8d0f029594dc1799f291da221fed
João Moreira <117107184+j38moreira@users.noreply.github.com>
2024-06-24 22:02:22 +0100

    update site

--- scripts.js
+++ scripts.js
@@ -1,23 +1,35 @@
 function showIcons() {
-    var iconContainer = document.getElementById("iconContainer");
-    if (iconContainer.style.display === "block") {
-      iconContainer.style.display = "none";
-    } else {
-      iconContainer.style.display = "block";
-    }
+  var iconContainerLeft = document.getElementById("iconContainerLeft");
+  var iconContainerRight = document.getElementById("iconContainerRight");
+  var avatar = document.querySelector(".avatar");
+
+  if (iconContainerLeft.style.display === "block" && iconContainerRight.style.display === "block") {
+      iconContainerLeft.style.display = "none";
+      iconContainerRight.style.display = "none";
+      avatar.classList.remove("no-blink");
+  } else {
+      iconContainerLeft.style.display = "block";
+      iconContainerRight.style.display = "block";
+      avatar.classList.add("no-blink");
+  }
 }
 
-var click = document.getElementById("click");
-var here = document.getElementById("here");
-var visible = true;
+document.addEventListener("DOMContentLoaded", function() {
+  var click = document.getElementById("click");
+  var here = document.getElementById("here");
+  var avatar = document.querySelector(".avatar");
+  var visible = true;
 
-setInterval(function () {
-  if (visible) {
-    click.style.visibility = "hidden";
-    here.style.visibility = "visible";
-  } else {
-    click.style.visibility = "visible";
-    here.style.visibility = "hidden";
-  }
-  visible = !visible;
-}, 1000);
+  setInterval(function () {
+      if (!avatar.classList.contains("no-blink")) {
+          if (visible) {
+              click.style.visibility = "hidden";
+              here.style.visibility = "visible";
+          } else {
+              click.style.visibility = "visible";
+              here.style.visibility = "hidden";
+          }
+          visible = !visible;
+      }
+  }, 1000);
+});
